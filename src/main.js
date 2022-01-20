f79034cac24f2b75c5883e5279bcd7a462fd43a5
kenoc1 <47333651+kenoc1@users.noreply.github.com>
2022-01-20 23:23:49 +0100

    changed default baseurl

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://apidjackets.codewithstein.com'
+//axios.defaults.baseURL = 'https://apidjackets.codewithstein.com'
+//axios.defaults.baseURL = 'localhost:8080'
+axios.defaults.baseURL = 'bi-backend.azurewebsites.net'
 
 createApp(App).use(store).use(router, axios).mount('#app')
