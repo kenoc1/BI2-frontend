b005b581952717429a792a3a50f1376cf41b2a3d
kenoc1 <47333651+kenoc1@users.noreply.github.com>
2022-01-20 23:44:53 +0100

    edited urls

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import axios from 'axios'
 
 //axios.defaults.baseURL = 'https://apidjackets.codewithstein.com'
-//axios.defaults.baseURL = 'localhost:8080'
-axios.defaults.baseURL = 'bi-backend.azurewebsites.net'
+//axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'https://bi-backend.azurewebsites.net'
 
 createApp(App).use(store).use(router, axios).mount('#app')
