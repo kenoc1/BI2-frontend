68f74358b4da1f393dcce5a2292f1ee0d0c49b85
kenoc1 <47333651+kenoc1@users.noreply.github.com>
2022-01-21 12:40:40 +0100

    deleted .env + edit .yml

--- src/main.js
+++ src/main.js
@@ -5,5 +5,8 @@ import store from './store'
 import axios from 'axios'
 
 axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
-
+//axios.defaults.baseURL = 'https://apidjackets.codewithstein.com'
+//axios.defaults.baseURL = 'http://localhost:8000'
+//axios.defaults.baseURL = 'https://bi-backend.azurewebsites.net'
+console.log(axios.defaults.baseURL)
 createApp(App).use(store).use(router, axios).mount('#app')
