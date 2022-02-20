904f5d70c8107d237f7393c0437650beb0372c45
fenja.tensing <fenja.tensing@openknowledge.de>
2022-02-20 19:09:00 +0100

    Added star-rating-component

---
+++ src/components/starRating.vue
@@ -0,0 +1,28 @@
+<template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <span v-for="index in 5" v-bind:class="{ checked: index <= rating }" class="fa fa-star fa-lg"></span>
+</template>
+
+<script>
+export default {
+  name: "starRating",
+  props:
+      ["productRating"],
+  data() {
+    return {
+      rating: this.productRating
+    }
+  },
+}
+</script>
+
+<style scoped>
+.checked {
+  color: orange !important;
+}
+
+.fa-star {
+  color: lightgray;
+}
+</style>
